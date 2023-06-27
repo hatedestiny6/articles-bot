0cfcc24c240151ebec74ff87ce9ebfab30ab19b3
viamn <skintolove123@gmail.com>
2023-06-27 13:25:09 +0200

    fix errors

--- assets/functions/checking/check_ozon.py
+++ assets/functions/checking/check_ozon.py
@@ -1,32 +1,26 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-
 def check_ozon(article_num):
-    """Вытаскивает цену из карточки товара с
-        артикулом article_num на ozon.ru
+    pass
+#     """Вытаскивает цену из карточки товара с
+#         артикулом article_num на ozon.ru
 
-    Args:
-        article_num (str): артикул товара
-    """
+#     Args:
+#         article_num (str): артикул товара
+#     """
 
-    browser = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    browser.set_window_size(800, 800)
+#     browser = webdriver.Chrome(
+#         service=ChromeService(ChromeDriverManager().install()))
+#     browser.set_window_size(800, 800)
 
-    browser.get(f"https://www.ozon.ru/product/{article_num}")
-    try:
-        price = browser.find_element(
-            By.CLASS_NAME, "kx6").text
+#     browser.get(f"https://www.ozon.ru/product/{article_num}")
+#     try:
+#         price = browser.find_element(
+#             By.CLASS_NAME, "kx6").text
 
-        with open("source.html", "w", encoding='utf-8') as f:
-            f.write(browser.page_source)
+#         with open("source.html", "w", encoding='utf-8') as f:
+#             f.write(browser.page_source)
 
-    except NoSuchElementException:
-        price = False
+#     except NoSuchElementException:
+#         price = False
 
-    browser.close()
-    return price
+#     browser.close()
+#     return price
--- assets/functions/parsing/parse_ozon.py
+++ assets/functions/parsing/parse_ozon.py
@@ -1,50 +1,44 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-
-
 def parse_ozon(article_num):
-    """Вытаскивает информацию из карточки товара с
-        артикулом article_num на ozon.ru
+    pass
+#     """Вытаскивает информацию из карточки товара с
+#         артикулом article_num на ozon.ru
 
-    Args:
-        article_num (str): артикул товара
-    """
+#     Args:
+#         article_num (str): артикул товара
+#     """
 
-    browser = webdriver.Chrome(
-        service=ChromeService(ChromeDriverManager().install()))
-    browser.maximize_window()
-    browser.get(f"https://www.ozon.ru/product/{article_num}")
+#     browser = webdriver.Chrome(
+#         service=ChromeService(ChromeDriverManager().install()))
+#     browser.maximize_window()
+#     browser.get(f"https://www.ozon.ru/product/{article_num}")
 
-    try:
-        brand = browser.find_element(
-            By.CLASS_NAME, "r4j").text
+#     try:
+#         brand = browser.find_element(
+#             By.CLASS_NAME, "r4j").text
 
-    except NoSuchElementException:
-        brand = "Неизвестно"
+#     except NoSuchElementException:
+#         brand = "Неизвестно"
 
-    try:
-        name = browser.find_element(
-            By.CLASS_NAME, "z9k").text
+#     try:
+#         name = browser.find_element(
+#             By.CLASS_NAME, "z9k").text
 
-    except NoSuchElementException:
-        name = "Неизвестно"
+#     except NoSuchElementException:
+#         name = "Неизвестно"
 
-    try:
-        discount = browser.find_element(
-            By.CLASS_NAME, "r2k").text[1:]
+#     try:
+#         discount = browser.find_element(
+#             By.CLASS_NAME, "r2k").text[1:]
 
-    except NoSuchElementException:
-        discount = False
+#     except NoSuchElementException:
+#         discount = False
 
-    try:
-        ozon_card = browser.find_element(
-            By.CLASS_NAME, "a1-a5").text
+#     try:
+#         ozon_card = browser.find_element(
+#             By.CLASS_NAME, "a1-a5").text
 
-    except NoSuchElementException:
-        ozon_card = False
+#     except NoSuchElementException:
+#         ozon_card = False
 
-    browser.close()
-    return [brand, name, discount, ozon_card]
+#     browser.close()
+#     return [brand, name, discount, ozon_card]
